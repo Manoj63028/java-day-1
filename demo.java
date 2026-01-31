98f6d0dfd17e4968b4c3ad38804b003fc7cfced4
MANOJ <kulagirimanojkumar12122007@gmail.com>
2026-01-31 14:30:57 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,11 @@
+class demo
+{
+     public static void main(String args[]){
+        System.out.println("hi");
+        System.out.print("hi");
+        System.out.println("hi");
+
+
+     }
+     
+}
